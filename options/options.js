7396b7d390b1370860a1495cf983d0942880f643
Crowdbotics <team@crowdbotics.com>
2023-04-26 00:04:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-bread-40221",
+  url: "https://soft-bread-40221.botics.co",
+  api: "https://soft-bread-40221.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
